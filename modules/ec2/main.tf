f8dbdfe059c7a3804c0c6f05a21680ee973af802
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-10-09 19:37:05 -0300

    security group ingress

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -46,6 +46,17 @@ resource "aws_security_group" "default" {
   }
 }
 
+resource "aws_security_group_rule" "ingress_internet" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = []
+  security_group_id = aws_security_group.default.id
+}
+
+
 resource "aws_security_group_rule" "egress_internet" {
   type              = "egress"
   from_port         = 0
